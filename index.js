73a283e3b4f1183d1520b1b0611e1538889393f9
Barend Du Toit <im@barend.io>
2019-08-21 14:03:59 -0600

    Inital commit

--- index.js
+++ index.js
@@ -12,29 +12,61 @@ class SC {
         .map(e => e.join("="))
         .join("&");
 
+      let url = `https://api-v2.soundcloud.com${type}?${urlParameters}`;
+
+      url = this.config.cors
+        ? "https://cors-anywhere.herokuapp.com/" + url
+        : url;
+
       axios({
-        url: `${
-          this.config.host
-        }/https://api-v2.soundcloud.com/${type}?${urlParameters}`
+        url: url,
+        headers: {
+          origin: "https://soundcloud.com"
+        }
       })
         .then(res => resolve(res.data))
         .catch(err => reject(err));
     });
   }
 
-  get(type, params) {
+  stream(type) {
     return new Promise((resolve, reject) => {
-      params.client_id = this.config.clientId;
-      let urlParameters = Object.entries(params)
-        .map(e => e.join("="))
-        .join("&");
-
       axios({
-        url: `${
-          this.config.host
-        }/https://api.soundcloud.com/i1/${type}?${urlParameters}`
+        url: `https://api.soundcloud.com/i1${type}?client_id=${
+          this.config.clientId
+        }`
       })
-        .then(res => resolve(res.data))
+        .then(res => {
+          let Track = new Audio(res.data.http_mp3_128_url);
+          Track.url = res.data.http_mp3_128_url;
+          Track.setVolume = vol => {
+            Track.volume = vol;
+          };
+          Track.isEnded = () => {
+            return Track.ended;
+          };
+          Track.getDuration = () => {
+            return Track.duration;
+          };
+          Track.seek = to => {
+            Track.currentTime = to;
+          };
+          Track.isPlaying = () => {
+            if (Track.defaultPlaybackRate > 0) {
+              return true;
+            } else {
+              return false;
+            }
+          };
+          Track.isActuallyPlaying = () => {
+            if (Track.defaultPlaybackRate > 0) {
+              return true;
+            } else {
+              return false;
+            }
+          };
+          resolve(Track);
+        })
         .catch(err => reject(err));
     });
   }
--- test.js
+++ test.js
@@ -1,12 +1,15 @@
 const SC = require(".");
 
 SC.init({
-  clientId: "tNdzqSQH10kJuLrRhPLbf5wtQEnaXmi1",
-  host: "https://aqueous-lake-30663.herokuapp.com"
+  clientId: "tNdzqSQH10kJuLrRhPLbf5wtQEnaXmi1"
 });
 
-SC.get("search/tracks", { q: "Crytix" })
-  .then(res => console.log(res))
-  .catch(err => {
-    console.log(err);
-  });
+// SC.get("/search/tracks", { q: "Crytix" })
+//   .then(res => console.log(res))
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+SC.stream("/tracks/93532103/streams").then(stream => {
+  console.log(stream);
+});
